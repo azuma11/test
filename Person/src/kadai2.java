9a4c0c6df440337ea56a7ca881d0823a8e6489bb
azuma11 <nana_0822@ezweb.ne.jp>
2018-11-19 21:44:58 +0900

    adwa

---
+++ Person/src/kadai2.java
@@ -0,0 +1,25 @@
+
+public class kadai2 {
+	public static void main(String[]args){
+		Person jiro = new Person();
+		jiro.name="木村次郎";
+		jiro.age=18;
+
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+
+		Person hanako = new Person();
+		hanako.name="鈴木花子";
+		hanako.age=16;
+
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
+		Person azuma = new Person();
+		azuma.name="東恭平";
+		azuma.age=25;
+
+		System.out.println(azuma.name);
+		System.out.println(azuma.age);
+	}
+}
