f2c4eebf9c4f3e31e2b896a4e2907651d84ad13c
azuma11 <dalu1sindo1@hotmail.co.jp>
2018-12-10 21:50:59 +0900

    commit

--- login/src/com/internousdev/login/dao/LoginDAO.java
+++ login/src/com/internousdev/login/dao/LoginDAO.java
@@ -14,8 +14,8 @@ public class LoginDAO {
 	String sql="select * from user where user_name=? and password=?";
 	try{
 		PreparedStatement ps=con.prepareStatement(sql);
-		ps.setString(1, name);
-		ps.setString(2, password);
+		ps.setString(1,name);
+		ps.setString(2,password);
 		ResultSet rs=ps.executeQuery();
 		if(rs.next()){
 			dto.setName(rs.getString("user_name"));
