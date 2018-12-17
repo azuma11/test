fa5fb5f7cee0b08b6681a743d02b09b52741e309
azuma11 <syunaoyuka@yahoo.co.jp>
2018-12-17 21:39:49 +0900

    commit

--- WebProj/src/com/internousdev/webproj/action/InquiryCompleteAction.java
+++ WebProj/src/com/internousdev/webproj/action/InquiryCompleteAction.java
@@ -13,4 +13,16 @@ public class InquiryCompleteAction extends ActionSupport{
 	public void setName(String name){
 		this.name=name;
 	}
+	public String getQtype(){
+		return qtype;
+	}
+	public void setQtype(String qtype){
+		this.qtype=qtype;
+	}
+	public String getBody(){
+		return body;
+	}
+	public void setBody(String body){
+		this.body=body;
+	}
 }
